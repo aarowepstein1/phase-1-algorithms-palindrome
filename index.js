edb6606eddf0fd04b3efa66f3e0450f1ee622cc2
Aarow Epstein <aarowepstein1@gmail.com>
2023-01-23 19:19:56 -0700

    commit tree 1efb4b8ce694480ff5e60b26e6db75cff93fc9e5

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const compareWord = word.split('').reverse().join('');
+  if (word === compareWord){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  need to be able to reverse the word then compare it to the original
+  result. 
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +25,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
